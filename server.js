8f841b567b203de80f868da343ffefbe17d5e741
Dhananjay <dhananjayman11@gmail.com>
2022-12-28 02:26:09 +0530

    cors

--- server.js
+++ server.js
@@ -1,8 +1,24 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
+const cors = require('cors');
 const PORT = process.env.PORT || 5000;
 
 const app = express();
+
+const corsOpts = {
+  origin: '*',
+
+  methods: [
+    'GET',
+    'POST',
+  ],
+
+  allowedHeaders: [
+    'Content-Type',
+  ],
+};
+
+app.use(cors(corsOpts));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
