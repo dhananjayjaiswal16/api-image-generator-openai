4c260afb674c3d79f14a9b3db9b59dd0abee81c9
Dhananjay <dhananjayman11@gmail.com>
2022-12-28 00:22:25 +0530

    openai api setup

--- controllers/generateImageController.js
+++ controllers/generateImageController.js
@@ -1,10 +1,32 @@
+import { Configuration, OpenAIApi } from "openai";
+const configuration = new Configuration({
+  apiKey: process.env.OPEN_AI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
+
 const generateImage = (req, res) => {
-  res.status(200).json({
-    sucess: true,
-    data: {
-      value: "fetched"
+  try {
+    const response = await openai.createImage({
+      prompt: "A cute baby sea otter",
+      n: 1,
+      size: "512x512",
+    });
+
+    const imageUrl = response.data.data[0].url;
+
+    res.status(200).json({
+      success: true,
+      image: imageUrl
+    });
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.status);
+      console.log(error.response.data);
+    } else {
+      console.log(error.message);
     }
-  })
+  }
 }
 
 module.exports = { generateImage };
